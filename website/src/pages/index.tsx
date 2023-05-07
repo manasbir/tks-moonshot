98314d0bb5e1017e6403ffed7e4fc9e599617be7
manas <bagrimanasbir@gmail.com>
2023-05-07 14:46:18 -0700

    a

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -5,12 +5,19 @@ import bg from "../../public/background.svg"
 import Header from '@/components/Header';
 import Welcome from '@/components/Welcome';
 import Information from '@/components/Information';
+import Head from 'next/head';
 const inter = Inter({ subsets: ['latin'] })
 
 
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>unai</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preload" href="/fonts/Inter-roman.var.woff2?v=3.19" as="font" type="font/woff2" crossOrigin="anonymous" />
+      
+    </Head>
     <main className={`selection:bg-opacity-80 selection:bg-pink-500 selection:text-white flex min-h-screen flex-col items-center ${inter.className}`}>
       <Header/>
       <Welcome/>
