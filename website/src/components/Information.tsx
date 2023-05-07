3df6181ea195d8926adf3d3ec10986d4e33e1a98
manas <bagrimanasbir@gmail.com>
2023-05-07 14:44:24 -0700

    a

--- website/src/components/Information.tsx
+++ website/src/components/Information.tsx
@@ -10,7 +10,7 @@ export default function Information(){
     return (
         <div className="flex flex-col items-center justify-center w-full h-full p-10">
             <div className="max-w-prose">
-                <h1 className='text-2xl'>How unai works</h1>
+                <h1 className='text-2xl'><b>How unai works</b></h1>
                 <p>UNAI injects events into your calendar based off your feedback and data. You can talk to UNAI like your personal assistant and it will do its best to make sure you are not wasting any time.</p>
             </div>
             <div className="max-w-prose mt-5">
